b351bb4b2165a30ff04ffcd97b7aa80e19696b33
Matheus Lascalla <matheusalg.nb@gmail.com>
2022-02-10 11:57:59 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,4 @@
-function onOff(){
-    document 
-        .querySelector("#modal")
-        .classList
-        .toggle("hide")
 
-    document
-        .querySelector("body")
-        .classList
-        .toggle("hideScroll")
-
-    document
-        .querySelector("#modal")
-        .classList
-        .toggle("addScroll")
-
-}
 
 function checkFields(event) {
 
