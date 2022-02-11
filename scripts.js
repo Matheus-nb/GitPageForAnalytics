5b1968ee21225e91a928a136e9c8f26188ba353b
Matheus Lascalla <matheusalg.nb@gmail.com>
2022-02-11 07:36:40 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function Avaliar(estrela) {
  var url = window.location;
  url = url.toString()
- url = url.split("Avaliação.html");
+ url = url.split("Avaliacao.html");
  url = url[0];
 
  var s1 = document.getElementById("s1").src;
